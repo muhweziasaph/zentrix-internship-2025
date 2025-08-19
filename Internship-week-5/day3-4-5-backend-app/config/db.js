a797b30f871016fb2b7c403945fbd3ac691bed26
MUHWEZI ASAPH <muhweziasaph33@gmail.com>
2025-08-19 03:34:08 +0300

    Update db.js

--- Internship-week-5/day3-4-5-backend-app/config/db.js
+++ Internship-week-5/day3-4-5-backend-app/config/db.js
@@ -1,15 +1,21 @@
+
 // config/db.js
 const mongoose = require('mongoose');
 async function connectDB() {
-  const mode = process.env.DB_MODE; // 'local' or 'atlas'
+  // Use Atlas if running in production, otherwise fallback to local
+  const mode = process.env.NODE_ENV === 'production' ? 'atlas' : 'local';
+  // Select URI based on mode
   const uri =
     mode === 'atlas' ? process.env.MONGO_URI_ATLAS : process.env.MONGO_URI_LOCAL;
   if (!uri) {
-    console.error('Missing MongoDB URI for selected DB_MODE in .env');
+    console.error('Missing MongoDB URI for the selected mode!');
     process.exit(1);
   }
   try {
-    await mongoose.connect(uri);
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log(`MongoDB connected (${mode})`);
   } catch (err) {
     console.error('MongoDB connection error:', err.message);
